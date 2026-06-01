2952023bfebf4c3e055bd7883e1533130e4deb75
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2026-06-01 00:42:46 -0300

    chore: alterar vite.config para cloudfare-pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig(
     {
         nitro: {
-            preset: "vercel",
+            preset: "cloudflare-pages",
         }
     }
 );
