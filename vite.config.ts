5b9a9aa1f214995779654dd5e524a0867f694fca
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2026-06-07 10:21:43 -0300

    fix: corrige erro de importação de CSS em produção

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,12 @@ import {defineConfig} from "vite";
 import {tanstackStart} from "@tanstack/react-start/plugin/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 import viteReact from "@vitejs/plugin-react";
+import tawindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
   plugins: [
+    tawindcss(),
     tanstackStart(),
     netlify(), 
     viteReact(),
